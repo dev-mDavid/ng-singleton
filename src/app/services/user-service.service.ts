f580719eb2e847fab67ce3e2ebee3ae9fe9ec795
mDavid <dev.mdavid@gmail.com>
2019-12-25 17:59:56 -0800

    config 2 types of users

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -4,6 +4,15 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  user: any;
+
+  setUser(newUser) {
+    this.user = newUser;
+  }
+
+  getUser(): any {
+    return this.user;
+  }
 
   constructor() { }
 }
