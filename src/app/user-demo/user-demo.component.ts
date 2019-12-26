c55b2c40d5c15b299511f1c069d4ed34fdf315f1
mDavid <dev.mdavid@gmail.com>
2019-12-25 18:19:22 -0800

    config UserDemo Controller to use User Service from Service Module

--- src/app/user-demo/user-demo.component.ts
+++ src/app/user-demo/user-demo.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ReflectiveInjector } from '@angular/core';
+import { UserService } from "../services/user-service.service";
 
 @Component({
   selector: 'app-user-demo',
   templateUrl: './user-demo.component.html',
   styleUrls: ['./user-demo.component.sass']
 })
-export class UserDemoComponent implements OnInit {
+export class UserDemoComponent {
+  userName: string;
+  userService: UserService;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() { 
+    const injector: any = ReflectiveInjector.resolveAndCreate([UserService]);
+    
+    this.userService = injector.get(UserService);
+  }
+  
+  signIn():void {
+    this.userService.setUser({
+      name: 'm David'
+    });
+    
+    this.userName = this.userService.getUser().name;
+    console.log('User name is:', this.userName);
   }
 
 }
