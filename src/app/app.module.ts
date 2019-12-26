b006a73465fc9314db622de4b09d5faa87d8e41a
mDavid <dev.mdavid@gmail.com>
2019-12-25 18:00:47 -0800

    generate UserDemo Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserDemoComponent } from './user-demo/user-demo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserDemoComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/user-demo/user-demo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-demo',
+  templateUrl: './user-demo.component.html',
+  styleUrls: ['./user-demo.component.sass']
+})
+export class UserDemoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
