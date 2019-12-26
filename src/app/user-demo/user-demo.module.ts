3b1789ffbad11dc1ea5d0814320c8e86c158c4bc
mDavid <dev.mdavid@gmail.com>
2019-12-25 19:14:35 -0800

    config UserDemo Module to inject UserService

--- src/app/user-demo/user-demo.module.ts
+++ src/app/user-demo/user-demo.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { UserService } from "../services/user-service.service";
 
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
-  ]
+  ],
+  providers: [
+    UserService
+  ],
+  declarations: []
 })
 export class UserDemoModule { }
